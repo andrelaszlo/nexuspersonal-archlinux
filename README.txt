401f496fe31b8e7629ef13bf012e732a85190ba5
André Laszlo <andre@laszlo.nu>
2011-06-28 00:46:36 +0200

    version 4.18.0.10751-3

--- README.txt
+++ README.txt
@@ -1,4 +1,9 @@
 make package:
 
 makepkg --source
-#https://install.bankid.com/sv/installbankidcom/Linuxsparet/Klientinstallation/
+https://install.bankid.com/sv/installbankidcom/Linuxsparet/Klientinstallation/
+
+The libpng12 dependency (with libpng installed only):
+
+$ /usr/local/bin/personal 
+/usr/local/lib/personal/personal.bin: error while loading shared libraries: libpng12.so.0: cannot open shared object file: No such file or directory
--- tools/check.sh
+++ tools/check.sh
@@ -8,6 +8,25 @@ then
 	md5sum LinuxPersonal.tgz > LinuxPersonal.md5
 	rm LinuxPersonal.tgz
 	echo "Done"
+elif [[ "$1" = "-p" ]]
+then
+	BANKID_URL="https://install.bankid.com/sv/installbankidcom/Linuxsparet/Klientinstallation/"
+	echo "Checking page for new version"
+	wget -q $BANKID_URL -O pagetemp.html --user-agent="Mozilla/5.0 (X11; U; Linux i686; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.475.0 Safari/534.3" --no-check-certificate
+	URLS=`egrep -o -a "location.href = '.*';" pagetemp.html | sed "s/location.href = '//g" | sed "s/';//g"`
+	echo $URLS > newurl.txt
+	diff newurl.txt lasturl.txt > /dev/null
+	if [ $? -eq 0 ]
+	then
+		echo "Still the same version"
+		exit 0
+	else
+		echo "New version!"
+		echo $URLS
+		echo "New version of NexusPersonal available at $URLS" | mail -s "New NexusPersonal version" andre@laszlo.nu
+		mv newurl.txt lasturl.txt
+		exit 1
+	fi
 elif [[ "$1" = "-c" ]]
 then
 	echo "Checking for new version"
@@ -25,5 +44,5 @@ then
 	fi
 	rm LinuxPersonal.tgz
 else
-	echo -e "Checks for new versions of the BankId application (Nexus Personal)\nUsage:\n\tcheck.sh -[c|u|h]\n\t  c Check if there is a new version\n\t  u Update cached version\n\t  h Show this message"
+	echo -e "Checks for new versions of the BankId application (Nexus Personal)\nUsage:\n\tcheck.sh -[c|p|u|h]\n\t  c Check if there is a new version\n\t  p Check NexusPersonal page for version\n\t  u Update cached version\n\t  h Show this message"
 fi

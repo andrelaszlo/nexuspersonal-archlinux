63a5a70d0e9d45acd4a6a14899dfd0389f0a6ce8
André Laszlo <andre@laszlo.nu>
2011-12-20 18:10:18 +0100

    new check script! 4.18.1.10899-3

--- tools/check.sh
+++ tools/check.sh
@@ -10,23 +10,35 @@ then
 	echo "Done"
 elif [[ "$1" = "-p" ]]
 then
-	BANKID_URL="https://install.bankid.com/sv/installbankidcom/Linuxsparet/Klientinstallation/"
 	echo "Checking page for new version"
-	wget -q $BANKID_URL -O pagetemp.html --user-agent="Mozilla/5.0 (X11; U; Linux i686; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.475.0 Safari/534.3" --no-check-certificate
-	URLS=`egrep -o -a "location.href = '.*';" pagetemp.html | sed "s/location.href = '//g" | sed "s/';//g"`
-	echo $URLS > newurl.txt
-	diff newurl.txt lasturl.txt > /dev/null
-	if [ $? -eq 0 ]
+
+	# old method: get download page and grep download url
+	#BANKID_URL="https://install.bankid.com/sv/installbankidcom/Linuxsparet/Klientinstallation/"
+	#wget -q $BANKID_URL -O pagetemp.html --user-agent="Mozilla/5.0 (X11; U; Linux i686; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.475.0 Safari/534.3" --no-check-certificate
+	#URLS=`egrep -o -a "location.href = '.*';" pagetemp.html | sed "s/location.href = '//g" | sed "s/';//g"`
+
+	# new method: get redirection url from download page
+	./getredir.expect &> /dev/null
+	URLS=$(egrep -o -a "href=\".*\"" http-redir.log | egrep -o -a "href=\"[^\"]*" *.log | sed "s/href=\"//g")
+	rm http-redir.log
+	if [[ "$URLS" != "" ]]
 	then
-		echo "Still the same version"
-		exit 0
+		echo $URLS > newurl.txt
+		diff newurl.txt lasturl.txt > /dev/null
+		if [ $? -eq 0 ]
+		then
+			echo "Still the same version"
+			exit 0
+		else
+			echo "New version!"
+			echo $URLS
+    		#echo "New version of NexusPersonal available at $URLS" | mail -s "New NexusPersonal version" andre@laszlo.nu
+			curl "http://andre.laszlo.nu/emailme.php?title=New%20NexusPersonal%20version&message=New%20version%20of%20NexusPersonal%20available%20at%20$URLS"
+			mv newurl.txt lasturl.txt
+			exit 1
+		fi
 	else
-		echo "New version!"
-		echo $URLS
-		#echo "New version of NexusPersonal available at $URLS" | mail -s "New NexusPersonal version" andre@laszlo.nu
-		curl "http://andre.laszlo.nu/emailme.php?title=New%20NexusPersonal%20version&message=New%20version%20of%20NexusPersonal%20available%20at%20$URLS"
-		mv newurl.txt lasturl.txt
-		exit 1
+		echo "Something went wrong. Either you're not connected or the download url changed."
 	fi
 elif [[ "$1" = "-c" ]]
 then
